e7bf4a011e55729f71eefeefb599d45cfabeeb32
Vladislav Ganshin <ganshinv@gmail.com>
2015-05-12 14:25:32 +0300

    forget

--- src/main/java/com/fivehundredtwelve/event/model/Session.java
+++ src/main/java/com/fivehundredtwelve/event/model/Session.java
@@ -1,4 +1,4 @@
-package com.fivehundredtwelve.event.model;
+git package com.fivehundredtwelve.event.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.sun.org.apache.xml.internal.dtm.ref.DTMDefaultBaseIterators;
